49e4e92289ea441e50fabd340e476623fce24017
jhunzik <jhunzik@users.noreply.github.com>
2022-08-14 16:49:42 -0700

    fix up neovim install on ubuntu 22.04

---
+++ roles/nvim/files/nvim/lua/base.lua
@@ -0,0 +1,26 @@
+vim.opt.encoding = 'utf-8'
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.rnu = true
+vim.opt.nu = true
+vim.opt.hlsearch = true
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.incsearch = true
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = 'no'
+vim.opt.colorcolumn = '120'
+vim.opt.showcmd = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.shell = 'zsh'
+vim.opt.backspace = 'start,eol,indent'
+vim.opt.path:append { '**' } -- subfolder search
