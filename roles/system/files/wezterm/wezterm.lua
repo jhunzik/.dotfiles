86d74d5259a0f3b7e9fc590f2eb27af9ef294dbd
Josh Hunziker <joshua.hunziker@octo.us>
2025-07-23 15:19:41 -0700

    catppuccin theme

--- roles/system/files/wezterm/wezterm.lua
+++ roles/system/files/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("Jetbrains Mono")
-config.color_scheme = "Catppuccin Frappe"
+config.color_scheme = "Catppuccin Mocha"
 config.enable_wayland = false
 
 return config
